ccab62b1cd5f3abad0c5e79edd79151755383722
Raul Moncayo <moncayoboy12@gmail.com>
2021-01-18 10:47:24 -0800

    Added comments and fixed formatting

--- README.md
+++ README.md
@@ -9,8 +9,8 @@
 			train/y_train.txt
 			activity_labels.txt
 			features.txt
-		The script reads in the previously mentioned files and performs the Data Cleanup/Modifications described in CodeBook.md.
-		The result of the script is, mergedTbl which is the tibble, dplyr data frame, of merged test and training datasets
-		with only the mean and standard deviation variables retained, and tidyTbl which is the average of all variables and rows in
-		mergedTbl grouped by subjectNumber and activity.
-		The tidy data is saved to 'out.txt' in the working directory.
+		The script reads in the previously mentioned files and performs the Data Cleanup/Modifications
+		described in CodeBook.md. The result of the script is, mergedTbl which is the tibble, dplyr 
+		data frame, of merged test and training datasets with only the mean and standard deviation variables
+		retained, and tidyTbl which is the average of all variables and rows in		mergedTbl grouped by 
+		subjectNumber and activity.	The tidy data dataframe is saved to 'out.txt' in the working directory.
--- run_analysis.R
+++ run_analysis.R
@@ -5,33 +5,42 @@ test <- read.table('test/X_test.txt', sep="")
 # Read in subject number data for each observation
 subject_train <- scan('train/subject_train.txt')
 subject_test <- scan('test/subject_test.txt')
-
-activity_labels <- sapply(strsplit(scan('activity_labels.txt', what="character", sep="\n"), " "), tail, 1)
+# Read in the activity label legend and strip line numbers
+activity_labels <- sapply(strsplit(scan('activity_labels.txt', what="character",
+                                        sep="\n"), " "), tail, 1)
+# Read in activity label numbers for each observation
 test_activity_labels <- scan('test/y_test.txt')
 train_activity_labels <- scan('train/y_train.txt')
-
+# Replace activity label numbers with the actual activity label string from 
+# activity_labels
 for(i in 1:length(activity_labels)) {
     test_activity_labels[test_activity_labels==i] <- activity_labels[i]
     train_activity_labels[train_activity_labels==i] <- activity_labels[i]
 }
-
+# Convert data frames to tidyverse tibble
 trainTbl <- tibble(train)
 testTbl <- tibble(test)
-
+# Get features/variable names and strip line numbers
 features <- scan('features.txt', character(), sep='\n')
 feat <- sapply(strsplit(features, " "), tail, 1)
+# Set variables in tibbles to features from features.txt
 names(trainTbl) <- feat
 names(testTbl) <- feat
-
+# Select only variables with mean and std in name. Filter tibbles to only mean 
+# and std variables
 meanNames <- feat[grep('mean', feat)]
 stdNames <- feat[grep('std', feat)]
 meanStdNames <- c(meanNames, stdNames)
 filteredTrainTbl <- trainTbl[meanStdNames]
 filteredTestTbl <- testTbl[meanStdNames]
-
-mutTrainTbl <- mutate(filteredTrainTbl, subjectNumber = subject_train, activity=train_activity_labels)
-mutTestTbl <- mutate(filteredTestTbl, subjectNumber = subject_test, activity=test_activity_labels)
-
+# add subjectNumber and activity columns to tibbles
+mutTrainTbl <- mutate(filteredTrainTbl, subjectNumber = subject_train,
+                      activity=train_activity_labels)
+mutTestTbl <- mutate(filteredTestTbl, subjectNumber = subject_test,
+                     activity=test_activity_labels)
+# Merge the training and test tibbles
 mergedTbl <- rbind(mutTrainTbl, mutTestTbl)
-tidyTbl <- group_by(mergedTbl, subjectNumber, activity) %>% summarise_all(list(mean=mean))
+# Group by subjectNumber and activity to calculate mean of all columns
+tidyTbl <- group_by(mergedTbl, subjectNumber, activity) %>% 
+    summarise_all(list(mean=mean))
 write.table(tidyTbl, row.names = FALSE, file='out.txt')
